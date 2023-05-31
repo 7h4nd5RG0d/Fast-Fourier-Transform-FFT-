a04269a0988e8431072e6b39a879daa566d77eb1
7h4nd5RG0d <128285431+7h4nd5RG0d@users.noreply.github.com>
2023-05-31 19:07:29 +0530

    Update FFT.py

--- FFT.py
+++ FFT.py
@@ -1,11 +1,4 @@
 # Fast-Fourier-Transform-(FFT)
-Fast polynomial multiplication
-
-**The forward transform:**
-Time Complexity = O(nlogn) where n os the degree of the polynomial 
-
-PYTHON CODE:
-
 **import math
 import cmath 
 import numpy as np
